c855512434c9f7b993cdee70c1c46c5e7269fd54
gus7avoh <72467759+gus7avoh@users.noreply.github.com>
2023-11-29 08:15:32 -0300

    Atualizar o 56.py

--- 56.py
+++ 56.py
@@ -27,19 +27,26 @@ for i in range(4):
    tupla=(idade[i],nome[i],sexos[i] )
    uniao.append(tupla)
 
+#pega as listas idade, nome e sexos e une todas em uma grande lista uniao
 
 for c in range(4):
    if uniao[c][2] == "mas":
        aa.append(uniao[c])
 
+#ele vai rodar 4 vezes para que o [c] seja alterado de 0 até 3, com isso ele vai validar se a posição 0,1,2,3 na posição 2 vai ser "mas", caso seja ele vai adicionar essa união na lista "aa" que eh preenchida somente por homens.
+
 for c in range(4):
    if uniao[c][2] == "fem":
        bb.append(uniao[c])
 
+#vai fazer a mesma coisa que o último for, porem vai ver se são mulheres e adicioná-la s na lista BB, que só tem mulheres.
 
-media = sum(idade)/4
-uniao_ordenada = tuple(sorted(uniao))
-aa_ordenado = tuple(sorted(aa, reverse=True))
+
+media = sum(idade)/4 #somatoria da lista idade e dividido por 4
+
+uniao_ordenada = tuple(sorted(uniao)) #responsavel por organizar a lista uniao
+
+aa_ordenado = tuple(sorted(aa, reverse=True)) #está pegando a união ordenada e organizando ela do maior para o menor.
 
 
 
